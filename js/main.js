1714571ee546ca9018e31d609bd1154860c3eb47
Steve Hu <yh3133@nyu.edu>
2022-07-02 18:23:12 -0400

    updates 0702

--- js/main.js
+++ js/main.js
@@ -2,7 +2,12 @@ $(function () {
   $("._footer").load("master_htmls/footer.html");
 
   setInterval(() => {
-    $("#someElement").fadeIn(200);
-    $("#someElement").fadeOut(200);
+    $("#flash").fadeIn(200);
+    $("#flash").fadeOut(200);
   }, 200);
+
+  setInterval(() => {
+    $(".blink").fadeIn(500);
+    $(".blink").fadeOut(500);
+  }, 500);
 });
