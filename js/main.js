e8b63debdb402fc21ca27c52640ab1548db20693
Steve Hu <yh3133@nyu.edu>
2022-07-11 16:15:20 -0400

    updates 0711.2

--- js/main.js
+++ js/main.js
@@ -10,4 +10,28 @@ $(function () {
     $(".blink").fadeIn(500);
     $(".blink").fadeOut(500);
   }, 500);
+
+  //Get the button:
+  mybutton = document.getElementById("back-to-top");
+
+  // When the user scrolls down 20px from the top of the document, show the button
+  window.onscroll = function () {
+    scrollFunction();
+  };
+
+  function scrollFunction() {
+    if (
+      document.body.scrollTop > 150 ||
+      document.documentElement.scrollTop > 150
+    ) {
+      mybutton.style.display = "block";
+    } else {
+      mybutton.style.display = "none";
+    }
+  }
+
+  $(mybutton).click(function () {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0;
+  });
 });
